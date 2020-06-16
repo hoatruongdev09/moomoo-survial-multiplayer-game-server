ea43f06018de8431b875cd2fe154f74269b8234b
Toan Ho <toanhodev@gmail.com>
2020-06-16 15:26:20 +0700

    .

---
+++ class/serverList.js
@@ -0,0 +1,106 @@
+const publicIp = require('public-ip');
+const axios = require('axios').default;
+const db = require("../models/index")
+
+const serverList = {
+    updateServerPasssword: "Dg;=8&gPZ%^e5q%",
+
+    serverList: {
+        local: "localhost:8080",
+        asia: "",
+        us: "moomoo-server.herokuapp.com",
+    },
+
+    getServerList: async function () {
+        for (var key in this.serverList) {
+            var model = await db.Url.findOne({
+                where: {
+                    name: key
+                }
+            }).then(model => {
+                // console.dir(model);
+                if (model != null) {
+                    console.log("model.dataValues.address ", model.dataValues.address)
+                    this.serverList[key] = model.dataValues.address + ":8080"
+                }
+            })
+            // console.dir(model);
+        }
+    },
+    // updateServer({
+    //     query: {
+    //         password: updateServerPasssword,
+    //         serverAddress: "54.169.71.33",
+    //         serverId: "asia"
+    //     }
+    // })
+
+    updateServer: function (req, res) {
+        console.log(`req.query.password == updateServerPasssword ${req.query.password == this.updateServerPasssword}`);
+        const serverId = req.query.serverId
+        if (serverId != null && req.query.password == this.updateServerPasssword) {
+            // serverList[serverId] = req.query.serverAddress + ":8080";
+            console.log(`updated ${this.serverList[req.query.serverId]}`);
+
+            this.updateOrCreate(db.Url, {
+                    name: serverId
+                }, {
+                    address: req.query.serverAddress,
+                    name: serverId
+                })
+                .then(function (result) {
+                    serverList.getServerList();
+                    result.item; // the model
+                    result.created; // bool, if a new item was created.
+                });
+        }
+        res.status(200);
+    },
+
+    updateOrCreate: async function (model, where, newItem) {
+        // First try to find the record
+        const foundItem = await model.findOne({
+            where
+        });
+        if (!foundItem) {
+            // Item not found, create a new one
+            const item = await model.create(newItem)
+            return {
+                item,
+                created: true
+            };
+        }
+        // Found an item, update it
+        const item = await model.update(newItem, {
+            where
+        });
+        return {
+            item,
+            created: false
+        };
+    },
+
+    updateIpAdressOnMainServer: async function () {
+        console.log(`pulic ip: ${await publicIp.v4()}`)
+        let ip = await publicIp.v4()
+        let query = {
+            serverId: 'asia',
+            serverAddress: ip
+        }
+        console.log(query);
+
+        axios.get('http://moomoo-server.herokuapp.com/update_server', {
+            params: {
+                serverId: 'asia',
+                serverAddress: ip,
+                password: "Dg;=8&gPZ%^e5q%"
+            }
+        }).then(() => {
+
+        }).catch((error) => {
+            console.log("error send update server ", error.response);
+
+        })
+    }
+}
+module.exports = serverList;
--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const Server = require('./class/server')
 const ServerConfig = require('./class/serverconfig')
 
-const publicIp = require('public-ip');
-const axios = require('axios').default;
-const jsonfile = require('jsonfile')
 const path = require('path')
 
-const serverListFile = path.join(__dirname, 'serverListFile.json')
-const db = require("./models/index")
+const serverList = require('./class/serverList')
 
 
 
@@ -20,13 +16,6 @@ const app = express()
 const web = require('http').Server(app)
 const PORT = process.env.PORT || 8080
 
-const updateServerPasssword = "Dg;=8&gPZ%^e5q%"
-
-var serverList = {
-    local: "localhost:8080",
-    asia: "54.169.71.33:8080",
-    us: "moomoo-server.herokuapp.com",
-}
 
 app.use(express.static(path.join(__dirname, 'Game')));
 
@@ -36,68 +25,13 @@ app.get('/', (req, res) => {
     res.status(200).send("OK")
 })
 app.get('/server_list', (req, res) => {
-    res.status(200).send(serverList)
+    res.status(200).send(serverList.serverList)
 })
 app.get('/update_server', (req, res) => {
-    updateServer(req, res);
+    serverList.updateServer(req, res);
 })
 console.log("express run on: ", listener.address().port)
 
-function getServerList() {
-    for (var key in serverList) {
-        db.Url.findAll({
-            where: {
-                name: key
-            }
-        }).then(model => {
-            console.dir(model)
-        })
-    }
-}
-
-getServerList()
-
-function updateServer(req, res) {
-    console.log(`req.query.password == updateServerPasssword ${req.query.password == updateServerPasssword}`);
-    const serverId = req.query.serverId
-    if (serverId != null && req.query.password == updateServerPasssword) {
-        serverList[serverId] = req.query.serverAddress + ":8080";
-        console.log(`updated ${serverList[req.query.serverId]}`);
-        db.Url.upsert({
-                address: req.query.serverAddress,
-                where: {
-                    name: serverId
-                }
-            })
-            .then(([urlObj, created]) => {
-                res.send("done")
-            });
-    }
-    res.status(200);
-}
-
-async function updateIpAdressOnMainServer() {
-    console.log(`pulic ip: ${await publicIp.v4()}`)
-    let ip = await publicIp.v4()
-    let query = {
-        serverId: 'asia',
-        serverAddress: ip
-    }
-    console.log(query);
-
-    axios.get('http://moomoo-server.herokuapp.com/update_server', {
-        params: {
-            serverId: 'asia',
-            serverAddress: ip,
-            password: "Dg;=8&gPZ%^e5q%"
-        }
-    }).then(() => {
-
-    }).catch((error) => {
-        console.log("error send update server ", error.response);
-
-    })
-}
-// updateIpAdressOnMainServer()
+serverList.getServerList();
 
 let server = new Server(ServerConfig, web)
